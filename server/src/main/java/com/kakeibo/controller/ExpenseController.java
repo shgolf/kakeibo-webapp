501c5e05353b9961e37ae4965e257bbfcf30d244
shgolf <shotax20a@g.ecc.u-tokyo.ac.jp>
2026-06-28 12:14:47 +0900

    feat: ExpenseControllerを修正し、ExpenseRequestとExpenseResponseを使用したバリデーションを追加

--- server/src/main/java/com/kakeibo/controller/ExpenseController.java
+++ server/src/main/java/com/kakeibo/controller/ExpenseController.java
@@ -1,7 +1,10 @@
 package com.kakeibo.controller;
 
+import com.kakeibo.dto.ExpenseRequest;
+import com.kakeibo.dto.ExpenseResponse;
 import com.kakeibo.model.Expense;
 import com.kakeibo.service.ExpenseService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,17 +22,20 @@ public class ExpenseController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Expense create(@RequestBody Expense expense) {
-        return expenseService.create(expense);
+    public ExpenseResponse create(@Valid @RequestBody ExpenseRequest request) {
+        Expense expense = request.toEntity();
+        return ExpenseResponse.from(expenseService.create(expense));
     }
 
     @GetMapping
-    public List<Expense> findAll() {
-        return expenseService.findAll();
+    public List<ExpenseResponse> findAll() {
+        return expenseService.findAll().stream()
+                .map(ExpenseResponse::from)
+                .toList();
     }
 
     @GetMapping("/id")
-    public Optional<Expense> findById(@RequestParam Long id) {
-        return expenseService.findById(id);
+    public Optional<ExpenseResponse> findById(@RequestParam Long id) {
+        return expenseService.findById(id).stream().map(ExpenseResponse::from).findFirst();
     }
 }
